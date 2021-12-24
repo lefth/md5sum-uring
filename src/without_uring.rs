924e464c7be65aee27d30700d572ef6c9a0c0dc3
Dan Zwell <devel@zwell.net>
2021-12-24 23:26:03 +0800

    When not using io_uring, mmap files instead of reading into a buffer
    That mode of operation is not really intended to be used, but mmap makes
    it work well with no extra effort.

--- src/without_uring.rs
+++ src/without_uring.rs
@@ -1,17 +1,17 @@
 /// This module uses calculates checksums without io_uring.
-use std::{fs::File, io::Read, path::PathBuf, sync::mpsc::Sender};
+use std::{fs::File, path::PathBuf, sync::mpsc::Sender};
 
 use anyhow::Result;
 use md5::{Digest, Md5};
+use memmap2::MmapOptions;
 
 pub fn get_checksums(files: Vec<PathBuf>, tx: Sender<(PathBuf, Result<Md5>)>) -> Result<()> {
     for path in files {
         let result = (|| {
-            let mut file = File::open(&path)?;
-            let mut buf = Vec::new();
-            file.read_to_end(&mut buf)?;
+            let file = File::open(&path)?;
             let mut md5 = Md5::new();
-            md5.update(&buf);
+            let mmap = unsafe { MmapOptions::new().map(&file)? };
+            md5.update(&mmap);
             Ok(md5)
         })();
         tx.send((path, result))?;
